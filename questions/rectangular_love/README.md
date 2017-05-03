46bdcbfb6a2c0f04439583d44558794c54281250
Daniel Tea <daniel_tea@spe.sony.com>
2017-05-03 11:45:24 -0700

    first pass

--- questions/rectangular_love/README.md
+++ questions/rectangular_love/README.md
@@ -1,1 +1,27 @@
 https://www.interviewcake.com/question/javascript/rectangular-love
+
+formatted as (leftX, bottomY, width, height)
+
+## ones with overlap
+1.
+r1 = 3,4,2,3
+r2 = 1,2,3,4
+
+=> 3,1,4,2
+
+2.
+r1 = 0,0,3,4
+r2 = 2,-2,2,4
+
+=> 2,0,1,2
+
+
+## ones without overlap
+1.
+r1 = 1,1,3,1
+r2 = 1,4,3,1
+
+2.
+r1 = 1,2,2,2
+r2 = 5,2,2,2
+
---
+++ questions/rectangular_love/love.js
@@ -0,0 +1,70 @@
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0;
+};
+
+function findXOverlap(r1, r2) {
+  let maxRHS = Math.max((r1.leftX + r1.width), (r2.leftX + r2.width));
+  let minLHS = Math.min(r1.leftX, r2.leftX);
+
+  if ((maxRHS - minLHS) < (r1.width + r2.width)) {
+    let leftX = Math.max(r1.leftX, r2.leftX);
+    let width = Math.min((r1.leftX + r1.width), (r2.leftX + r2.width)) - leftX;
+
+    return {
+      leftX: leftX,
+      width: width,
+    };
+  }
+
+  return {};
+};
+
+function findYOverlap() {
+  let maxRHS = Math.max((r1.bottomY + r1.height), (r2.bottomY + r2.height));
+  let minLHS = Math.min(r1.bottomY, r2.bottomY);
+
+  if ((maxRHS - minLHS) < (r1.height + r2.height)) {
+    let bottomY = Math.max(r1.bottomY, r2.bottomY);
+    let height = Math.min((r1.bottomY + r1.height), (r2.bottomY + r2.height)) - bottomY;
+
+    return {
+      bottomY: bottomY,
+      height: height,
+    };
+  }
+
+  return {};
+};
+
+let r1 = {
+  leftX: 3,
+  bottomY: 4,
+
+  width: 2,
+  height: 3,
+};
+
+let r2 = {
+  leftX: 1,
+  bottomY: 2,
+
+  width: 3,
+  height: 4,
+};
+
+let xCoords = findXOverlap(r1, r2);
+let yCoords = findYOverlap(r1, r2);
+
+if (isEmpty(xCoords) || isEmpty(yCoords)) {
+  console.log('no overlap!');
+} else {
+  let intersectedRectangle = {
+    leftX: xCoords.leftX,
+    bottomY: yCoords.bottomY,
+
+    width: xCoords.width,
+    height: yCoords.height,
+  }
+
+  console.log(intersectedRectangle);
+}
