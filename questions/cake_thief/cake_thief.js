bb6c416a43d97d22ea2bf79732a66da1410eb974
Daniel Tea + William Andrews <daniel_tea@spe.sony.com>
2017-05-31 22:31:12 -0700

    first pass

--- questions/cake_thief/cake_thief.js
+++ questions/cake_thief/cake_thief.js
@@ -5,8 +5,45 @@ class Cake {
   }
 }
 
+const calculateValue = (cake) => {
+  if (cake.weight === 0) {
+    return 0;
+  }
+
+  return cake.value / cake.weight;
+};
+
+const compare = (a,b) => {
+  if (calculateValue(a) < calculateValue(b)) {
+    return 1;
+  }
+
+  if (calculateValue(a) > calculateValue(b)) {
+    return -1;
+  }
+
+  return 0;
+}
+
 const maxDuffelBagValue = (cakes, capacity) => {
-  return 555;
+  let toKeep = [];
+  let currentWeight = 0;
+  let currentCake;
+
+  let sorted = cakes.sort(compare);
+
+  for (var i = 0; i < sorted.length; i++) {
+    currentCake = sorted[i];
+
+    while (currentWeight + currentCake.weight<= capacity) {
+      toKeep.push(currentCake);
+      currentWeight = currentWeight + currentCake.weight;
+    }
+  }
+
+  return toKeep.reduce((acc, cake) => {
+    return acc + cake.value
+  }, 0);
 };
 
 let cakes = [
